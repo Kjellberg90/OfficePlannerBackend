14516f22d52ae71291eb92d320e280ba4c68c91b
Emil Johansson <emil.johansson2@epiroc.com>
2023-01-16 12:06:51 +0100

    Added basic structure with Get-requests.

---
+++ SKY_Backend/SKY_Backend/Controllers/GroupController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace SKY_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        [HttpGet("get-groups")]
+        public IActionResult GetGroups()
+        {
+            IGroupService groupService = new GroupService();
+
+            try
+            {
+                var result = groupService.GetGroups();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("get-group")]
+        public IActionResult GetGroup(int id)
+        {
+            IGroupService groupService = new GroupService();
+
+            try
+            {
+                var result = groupService.GetGroup(id);
+
+                if(result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ SKY_Backend/SKY_Backend/Controllers/RoomsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Service;
+
+namespace SKY_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet("get-rooms")]
+        public IActionResult GetRooms()
+        {
+            IRoomService roomService = new RoomService();
+
+            try
+            {
+                var result = roomService.GetRooms();
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("get-room")]
+        public IActionResult GetRoom(int roomId)
+        {
+            IRoomService roomService = new RoomService();
+            
+            try
+            {
+                var result = roomService.GetRoom(roomId);
+                if (result == null)
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
