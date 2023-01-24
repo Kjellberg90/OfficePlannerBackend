b04f0a66632acc6a0a0fb6f0cd4c380a2cedd1c5
Emil Johansson <emil.johansson2@epiroc.com>
2023-01-24 14:12:02 +0100

    Added Bookingmodel and Booking.json

---
+++ SKY_Backend/DAL/Models/Booking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+        public int DayNr { get; set; }
+        public int? United { get; set; }
+        public int? Innovation { get; set; }
+        public int? Committment { get; set; }
+        public int? Collaboration { get; set; }
+        public int? InspiredA { get; set; }
+        public int? InspiredB { get; set; }
+
+    }
+}
