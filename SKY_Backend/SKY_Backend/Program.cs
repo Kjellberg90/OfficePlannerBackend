2df017e0be1d4571f6d53c777f31b77fd0b074fc
Daniel Kjellberg <danielkjellberg@hotmail.com>
2023-03-14 09:04:15 +0100

    Added options to Swagger for authentication

--- SKY_Backend/SKY_Backend/Program.cs
+++ SKY_Backend/SKY_Backend/Program.cs
@@ -1,7 +1,9 @@
 using DAL;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Service;
+using Swashbuckle.AspNetCore.Filters;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +13,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Role Based Authentication",
+        In = ParameterLocation.Header,
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey
+    });
+
+    options.OperationFilter<SecurityRequirementsOperationFilter>();
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "Cors", policy =>
