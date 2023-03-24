e2c854ffff9ca30f8cbe7594fb90e9588ec521af
Emil Johansson <emil.johansson2@epiroc.com>
2023-03-24 08:55:48 +0100

    Fixed Inspired/AB for Rooms

--- SKY_Backend/DAL/Models/SingleBooking.cs
+++ SKY_Backend/DAL/Models/SingleBooking.cs
@@ -12,5 +12,6 @@ namespace DAL.Models
         public string? Name { get; set; }
         public DateTime Date { get; set; }
         public Room BookedRoom { get; set; }
+        public string Password { get; set; }
     }
 }
